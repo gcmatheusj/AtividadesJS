64838245d34b7dc933579bbff1cd81a7bd9accca
Matheus Castro <goca1377@gmail.com>
2018-07-15 16:33:52 -0300

    commit slider

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -9,4 +9,10 @@ app.post('/usuarios', (req, res) => {
     res.send('<h1>Parabéns</h1>')
 })
 
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns usuário alterado</h1>')
+})
+
 app.listen(3003)
